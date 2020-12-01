b426de412700c7ef8cd94d7e245a30b41e25eebd
Korti <lucas.korthauer@gmail.com>
2020-12-01 18:29:05 +0100

    Solution day 1 with itertools

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,31 +1,13 @@
 const { loadData, writeData } = require('../util/data-provider');
+const { combinations } = require('iter-tools');
 
 function execute(level) {
     const inputData = loadData(1, level).map(d => parseInt(d, 10));
 
-    // Maybe not the cleanest or prettiest solution but who cares 😂
-    if(level == 1) {
-        for(let i = 0; i < inputData.length; i++) {
-            for(let j = i + 1; j < inputData.length; j++) {
-                const sum = inputData[i] + inputData[j];
-                if(sum === 2020) {
-                    writeData(1, level, inputData[i] * inputData[j]);
-                    return;
-                }
-            }
-        }
+    if(level === 1) {
+        for(let c of combinations(inputData, 2)) { if(c[0] + c[1] === 2020) { writeData(1, level, c[0] * c[1]); return; } }
     } else if(level === 2) {
-        for(let i = 0; i < inputData.length; i++) {
-            for(let j = i + 1; j < inputData.length; j++) {
-                for(let k = j + 1; k < inputData.length; k++) {
-                    const sum = inputData[i] + inputData[j] + inputData[k];
-                    if(sum === 2020) {
-                        writeData(1, level, inputData[i] * inputData[j] * inputData[k]);
-                        return;
-                    }
-                }
-            }
-        }
+        for(let c of combinations(inputData, 3)) { if(c[0] + c[1] + c[2] === 2020) { writeData(1, level, c[0] * c[1] * c[2]); return; } }
     }
 }
 
